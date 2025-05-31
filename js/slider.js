9b652092b65d55d8b44841c969fdcadfada1fad9
IvanZakharov77 <ivan.zzaharov@gmail.com>
2025-05-31 14:38:44 +0300

    second slider

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,4 @@
+// first slider
 let parents = document.getElementsByClassName('hero');
 
 const disableScroll = function (event) {
@@ -10,7 +11,7 @@ const disableTouchMove = function (event) {
 
 let currentSlide = 1;
 const slides = document.querySelectorAll('.slid');
-const container = document.querySelector('.hero');
+
 //
 let circles = document.querySelectorAll('.circle-hero');
 let lastScroll = 0;
@@ -139,7 +140,7 @@ const rightBtn = document.querySelector('.right-btn');
 
 rightBtn.addEventListener('click', function () {
   if (currentSlide === 1) {
-    return; // Ничего не делаем, если currentSlide равен 1
+    return;
   }
 
   changeSlideBackward();
@@ -152,7 +153,7 @@ rightBtn.addEventListener('click', function () {
 
 leftBtn.addEventListener('click', function () {
   if (currentSlide === 5) {
-    return; // Ничего не делаем, если currentSlide равен 1
+    return;
   }
 
   changeSlideForward();
@@ -167,4 +168,132 @@ leftBtn.addEventListener('click', function () {
 for (let i = 0; i < parents.length; i++) {
   parents[i].addEventListener('mouseenter', handleMouseEnter);
   parents[i].addEventListener('mouseleave', handleMouseLeave);
+}
+
+// ---------------------second slider
+
+let parentsThird = document.getElementsByClassName('third-block');
+
+const disableScrollThird = function (event) {
+  event.preventDefault();
+};
+
+const disableTouchMoveThird = function (event) {
+  event.preventDefault();
+};
+
+let currentSlideThird = 1;
+const slidesThird = document.querySelectorAll('.slider-t');
+
+function changeSlideForwardThird() {
+  const current = document.querySelector(`.slider-animation${currentSlideThird}`);
+  if (current) {
+    current.style.transition = 'transform 1s ease, opacity 1s ease';
+    current.style.transform = 'translateX(-100%)';
+    current.style.opacity = 0;
+
+    setTimeout(() => {
+      current.style.display = 'none';
+    }, 1000);
+  }
+
+  currentSlideThird++;
+  if (currentSlideThird > slidesThird.length) {
+    currentSlideThird = 1;
+  }
+  console.log(currentSlideThird);
+
+  const next = document.querySelector(`.slider-animation${currentSlideThird}`);
+  if (next) {
+    next.style.right = '100px';
+    next.style.display = 'inline-flex';
+    next.style.transition = 'transform 1s ease, opacity 1s ease';
+    next.style.transform = 'translateX(100%)';
+    next.style.opacity = 1;
+
+    setTimeout(() => {
+      next.style.transform = 'translateX(0)';
+    }, 0);
+  }
+}
+
+function changeSlideBackwardThird() {
+  const current = document.querySelector(`.slider-animation${currentSlideThird}`);
+  if (current) {
+    current.style.transition = 'transform 1s ease, opacity 1s ease';
+    current.style.transform = 'translateX(100%)';
+    current.style.opacity = 0;
+
+    setTimeout(() => {
+      current.style.display = 'none';
+    }, 1000);
+  }
+  //
+
+  //
+  currentSlideThird--;
+  if (currentSlideThird < 1) {
+    currentSlideThird = slidesThird.length;
+  }
+
+  const previous = document.querySelector(`.slider-animation${currentSlideThird}`);
+  if (previous) {
+    previous.style.display = 'inline-flex';
+    previous.style.transition = 'transform 1s ease, opacity 1s ease';
+    previous.style.transform = 'translateX(-100%)';
+    previous.style.opacity = 1;
+
+    setTimeout(() => {
+      previous.style.transform = 'translateX(0)';
+    }, 0);
+  }
+}
+
+function startPageScrollThird() {
+  window.removeEventListener('wheel', handleScrollSlide);
+  window.removeEventListener('wheel', handleScrollSlide);
+  window.removeEventListener('touchmove', disableTouchMove);
+}
+
+function startSlideScrollThird() {
+  window.removeEventListener('wheel', disableScroll);
+  window.removeEventListener('wheel', handleScrollSlideThird);
+  window.removeEventListener('touchmove', disableTouchMove);
+}
+
+function handleScrollSlideThird(event) {
+  event.preventDefault();
+
+  if (event.deltaY > 0) {
+    changeSlideForwardThird();
+
+    if (currentSlideThird === 3) {
+      startPageScroll();
+    }
+  } else if (event.deltaY < 0 && currentSlideThird > 1) {
+    changeSlideBackwardThird();
+
+    if (currentSlideThird === 1) {
+      startPageScroll();
+    }
+  }
+}
+
+function handleMouseEnterThird(event) {
+  window.addEventListener('wheel', handleScrollSlideThird, { passive: false });
+  window.addEventListener('touchmove', disableTouchMove, { passive: false });
+}
+
+function handleMouseLeaveThird(event) {
+  window.removeEventListener('wheel', handleScrollSlideThird);
+  window.removeEventListener('touchmove', disableTouchMove);
+
+  if (currentSlideThird === 3) {
+    startPageScroll();
+  }
+}
+
+for (let i = 0; i < parentsThird.length; i++) {
+  parentsThird[i].addEventListener('mouseenter', handleMouseEnterThird);
+  parentsThird[i].addEventListener('mouseleave', handleMouseLeaveThird);
 }
